64582f6204375f8240d06b68147e4777694dfb19
personNumber8 <33554571+personNumber8@users.noreply.github.com>
2018-10-20 16:33:04 -0400

    Update QuadSolver.c
    changed the psudo code into c code

--- QuadSolver.c
+++ QuadSolver.c
@@ -1,4 +1,33 @@
-QuadSolver 
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+int main(int argc, char const *argv[]) {
+  float a= atof(argv[1]);
+  float b= atof(argv[2]);
+  float c= atof(argv[3]);
+  double discriminant=0;
+  discriminant =  b*b-4*a*c;
+  double Root1=0;
+  double Root2=0;
+  printf("%f\n",a );
+  printf("%f\n",b );
+  printf("%f\n",c );
+  printf("%f\n",discriminant );
+  if (discriminant>0){
+     Root1 = (-b+sqrt(discriminant) )/(2*a);
+     Root2 = (-b-sqrt(discriminant))/(2*a);
+
+  }
+  else if (discriminant ==0){
+    Root1 = Root2 = -b/(2*a);
+
+  }
+  printf("%f\n",Root1 );
+  printf("%f\n",Root2 );
+  return 0;
+}
+
+/*QuadSolver 
 Define double a, b, c, root1, root2, discriminant 
   Get a,b,c from user 
   discriminant =  b*b-4*a*c 
@@ -12,3 +41,6 @@ Define double a, b, c, root1, root2, discriminant
   End If 
 //code has not taken into factor of roots that are not real 
 End QuadSolver
+*/
+
+
