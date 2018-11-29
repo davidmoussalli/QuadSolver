20014b34e2ab93df1ea8b74821469bb8846d4486
personNumber8 <33554571+personNumber8@users.noreply.github.com>
2018-11-29 16:45:49 -0500

    updated input

--- quadsolver/input/input.c
+++ quadsolver/input/input.c
@@ -3,14 +3,12 @@
 #include <stdlib.h>
 #include <math.h>
 #include <ctype.h>
+#include <string.h>
 
+char* inputs()
+  {
+  char *UserInput=malloc(30);
+  fgets(UserInput,29,stdin);
 
-float * inputs(char const *input1,char const *input2,char const *input3) {
-
-float A_B_C[3];
-    A_B_C[0]= atof(input1);
-    A_B_C[1]= atof(input2);
-    A_B_C[2]= atof(input3);
-printf("%f %f %f\n",A_B_C[0],A_B_C[1],A_B_C[2] );
-return A_B_C;
-}
+return UserInput;
+  }
