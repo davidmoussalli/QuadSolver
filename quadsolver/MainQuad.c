48f5df27eb53a5ea8487e1303d31f479ead57295
safwanzulfazli <44173108+safwanzulfazli@users.noreply.github.com>
2018-11-29 15:11:16 -0500

    Added copyright GNU

--- quadsolver/MainQuad.c
+++ quadsolver/MainQuad.c
@@ -69,3 +69,15 @@ Define double a, b, c, root1, root2, discriminant
 //code has not taken into factor of roots that are not real
 End QuadSolver
 */
+@c -*-texinfo-*-
+@c This is part of the GNU Emacs Lisp Reference Manual.
+@c Copyright (C) 1990, 1991, 1992, 1993, 1994, 1999
+@c   Free Software Foundation, Inc.
+@c See the file elisp.texi for copying conditions.
+@c Copyright (C)  2018  Safwan Kadir, Dakota Grant, David Moussalli.
+  Permission is granted to copy, distribute and/or modify this document
+  under the terms of the GNU Free Documentation License, Version 1.3
+  or any later version published by the Free Software Foundation;
+  with no Invariant Sections, no Front-Cover Texts, and no Back-Cover
+  Texts.  A copy of the license is included in the section entitled ``GNU
+  Free Documentation License''.
